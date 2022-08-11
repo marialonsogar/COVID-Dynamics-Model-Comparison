7d66a8cfbc5d323d031ae24e78bb1753da8e5baa
marialonsogar <marialonsogar@usal.es>
2022-08-11 14:24:02 +0200

    general update

--- modeling/utils.py
+++ modeling/utils.py
@@ -319,3 +319,35 @@ def plot_all_predictions(y_fit_train, y_fit_test, df, model_name='',window_size=
     ax2.set_ylim(ax1.get_ylim())
 
     plt.show()
+
+def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+    """Show df as figure"""
+
+    data = data.round(2)
+    
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, rowLabels=data.index, **kwargs)
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in mpl_table._cells.items():
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    return ax.get_figure(), ax
+
+def show_eval_table_and_save(data, path_to_save_image=None):
+    """Show df as figure and save"""
+    fig, ax = render_mpl_table(data, header_columns=0, col_width=2.0)
+
+    if path_to_save_image is not None:
+        fig.savefig(path_to_save_image)
